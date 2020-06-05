a49705c09d67a34064c6b05098cd1015ff63ab9b
Your Name <you@example.com>
2020-06-05 15:52:20 +0100

    Install FCC test suite Change arrows to buttons Create ControlBlock component, add Reset method and outline startStop method

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { faPause } from '@fortawesome/free-solid-svg-icons'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
 import { faSyncAlt } from '@fortawesome/free-solid-svg-icons'
+import ReactFCCtest from 'react-fcctest';
 
 class LengthBlock extends React.Component {
 	constructor(props) {
@@ -14,43 +15,115 @@ class LengthBlock extends React.Component {
 	}
 
 	render() {
-
 		return (
 				<div id={this.props.title.toLowerCase().concat("-container")}> 
 					<div id={this.props.title.toLowerCase().concat("-label")}>{this.props.title} Length</div>
-					<span id={this.props.title.toLowerCase().concat("-decrement")} onClick={this.props.decreaseHandler}><FontAwesomeIcon icon={faArrowDown} className="icon-Size"/></span> 
-					<span id={this.props.title.toLowerCase().concat("-length")}>{this.props.value}</span> 
-					<span id={this.props.title.toLowerCase().concat("-increment")} onClick={this.props.increaseHandler}><FontAwesomeIcon icon={faArrowUp} className="icon-Size"/></span>
+					<button id={this.props.title.toLowerCase().concat("-decrement")} onClick={this.props.decreaseHandler}><FontAwesomeIcon icon={faArrowDown} className="icon-Size"/></button> 
+					<div id={this.props.title.toLowerCase().concat("-length")}>{this.props.value}</div>
+					<button id={this.props.title.toLowerCase().concat("-increment")} onClick={this.props.increaseHandler}><FontAwesomeIcon icon={faArrowUp} className="icon-Size"/></button> 
 				</div>
 			);
 	}
 }
+
+class ControlBlock extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+				<div id="control-container">
+					<button id="start_stop" onClick={this.props.stopStartHandler}><FontAwesomeIcon icon={faPlay} className="icon-Size" /><FontAwesomeIcon icon={faPause} className="icon-Size" /></button> 
+					<button id="reset" onClick={this.props.resetHandler}><FontAwesomeIcon icon={faSyncAlt} className="icon-Size" /></button>
+				</div>
+			);
+	}
+}
+
+class Timer extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+				<div id="timer-container">
+						<div id="timer-label">Session/Break</div>
+						<span id="time-left">25:00</span>
+				</div>
+			);
+	}
+}
+
 
 class PomodoroClock extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			break: 5,
-			session: 25
-		}
+			breakLength: 5,
+			sessionLength: 25,
+			countDown: false,
+
+		};
+		this.increaseBreak = this.increaseBreak.bind(this);
+		this.increaseSession = this.increaseSession.bind(this);
+		this.decreaseBreak = this.decreaseBreak.bind(this);
+		this.decreaseSession = this.decreaseSession.bind(this);
+		this.startStop = this.startStop.bind(this);
+		this.reset = this.reset.bind(this);
 	}
 
 	increaseBreak() {
+		const breakUpdate = this.state.breakLength+1;
 
+		if(breakUpdate <=60) {
+			this.setState({
+				breakLength: breakUpdate
+			})
+		}
 	}
 
 	increaseSession() {
+		const sessionUpdate = this.state.sessionLength+1;
 
+		if(sessionUpdate <=60) {
+			this.setState({
+				sessionLength: sessionUpdate
+			})
+		}
 	}
 
-
 	decreaseBreak() {
+		const breakUpdate = this.state.breakLength-1;
 
+		if(breakUpdate >=1) {
+			this.setState({
+				breakLength: breakUpdate
+			})
+		}
 	}
 
 	decreaseSession() {
+		const sessionUpdate = this.state.sessionLength-1;
 
+		if(sessionUpdate >=1) {
+			this.setState({
+			sessionLength: sessionUpdate
+			})
+		}
+	}					
+
+	startStop() {
+
+	}
+
+	reset() {
+		this.setState({
+			breakLength: 5,
+			sessionLength: 25
+		})
 	}
 
 	render() {
@@ -58,17 +131,12 @@ class PomodoroClock extends React.Component {
 				<React.Fragment>
 					<div id="container">
 							<h1>Pomodoro Clock</h1>
-									<LengthBlock title="Break" value={this.state.break} increaseHandler={this.increaseBreak} decreaseHandler={this.decreaseBreak} />
-									<LengthBlock title="Session" value={this.state.session} increaseHandler={this.increaseSession} decreaseHandler={this.decreaseSession} />
-							<div id="timer-container">
-									<div id="timer-label">Session/Break</div>
-									<span id="time-left">25:00</span>
-							</div>
-
-							<div id="control-container">
-									<span id="start-stop"> <FontAwesomeIcon icon={faPlay} className="icon-Size" /><FontAwesomeIcon icon={faPause} className="icon-Size" /> </span> <span id="reset"> <FontAwesomeIcon icon={faSyncAlt} className="icon-Size" /></span>
-							</div>
+									<LengthBlock title="Break" value={this.state.breakLength} increaseHandler={this.increaseBreak} decreaseHandler={this.decreaseBreak} />
+									<LengthBlock title="Session" value={this.state.sessionLength} increaseHandler={this.increaseSession} decreaseHandler={this.decreaseSession} />
+							<Timer status={this.state.countDown} />
+							<ControlBlock stopStartHandler={this.startStop} resetHandler={this.reset} />
 					</div>
+					<ReactFCCtest />
 				</React.Fragment>
 			);
 	}
