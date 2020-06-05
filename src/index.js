9743b368cdfaf2bb0a73585685239238bb6dc0a4
Your Name <you@example.com>
2020-06-05 10:20:24 +0100

    Initial commit, basic, static layout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class PomodoroClock extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+				<React.Fragment>
+					<div id="container">
+							<h1>Pomodoro Clock</h1>
+
+							<div id="break-container"> 
+									<div id="break-label">Break Length></div>
+									<span id="break-decrement">-</span><span id="break-length">5</span><span id="break-increment">+</span>
+							</div>
+							<div id="session-container">
+									<div id="session-label">Session Length></div>
+									<span id="session-decrement">-</span><span id="break-length">25</span><span id="session-increment">+</span>
+							</div>
+
+							<div id="timer-container">
+									<div id="timer-label">Session/Break</div>
+									<span id="time-left">25:00</span>
+							</div>
+
+							<div id="control-container">
+								<span id="start-stop"> > || </span> <span id="reset"> reset</span>
+							</div>
+					</div>
+				</React.Fragment>
+			);
+	}
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PomodoroClock />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
