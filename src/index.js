433042408822db4b1f872dad98bdfbf1a89fbb11
Kay Pea <19981420+ravenblack24@users.noreply.github.com>
2020-09-30 15:45:17 +0100

    image update, readme

--- src/index.js
+++ src/index.js
@@ -2,18 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import PomodoroClock from './components/PomodoroClock';
-import ReactFCCtest from 'react-fcctest';
 import {Helmet} from "react-helmet";
 
 ReactDOM.render(
   <React.StrictMode>
 	<Helmet>
 		<meta charSet="utf-8" />
-		<title>ReactJS - Pomodoro Clock</title>
+		<title>Pomodoro Clock - React.js app</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	</Helmet>
     <PomodoroClock />
-	<ReactFCCtest />
   </React.StrictMode>,
   document.getElementById('root')
 );
